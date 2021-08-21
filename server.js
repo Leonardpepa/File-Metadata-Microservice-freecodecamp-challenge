d2ff24f9cf6e8127b2d8a2c9f13f7dcde2c21ce8
Leonard Pepa <leopepp4@gmail.com>
2021-08-21 03:48:02 +0300

    completed the challenge

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const multiparty = require('multiparty');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
-app.use(cors());
+app.use(cors({optionsSuccessStatus: 200}));
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json({extended: false}));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse", (req, res) => {
+  var form = new multiparty.Form();
+  form.parse(req, function(err, fields, files) {
+    res.json({
+      name: files.upfile[0].originalFilename,
+      type: files.upfile[0].headers['content-type'],
+      size: files.upfile[0].size
+    })
+  });  
 });
 
 
